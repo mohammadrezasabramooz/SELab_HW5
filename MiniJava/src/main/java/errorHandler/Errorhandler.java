5f66a0f6b22121c25123820e1e466662c8501977
mohammadrezasabramooz <34890448+mohammadrezasabramooz@users.noreply.github.com>
2020-08-19 14:23:49 +0430

    Update Errorhandler.java

--- MiniJava/src/main/java/errorHandler/Errorhandler.java
+++ MiniJava/src/main/java/errorHandler/Errorhandler.java
@@ -3,7 +3,7 @@ package errorHandler;
 /**
  * Created by Alireza on 6/28/2015.
  */
-public class ErrorhandlerClass {
+public class ErrorhandlerHelper {
     public static boolean hasError = false;
 
     public static void printError(String msg) {
