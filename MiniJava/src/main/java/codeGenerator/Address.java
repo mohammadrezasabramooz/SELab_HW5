8ed11295faa0669ac5a552fc00e75801ca928df0
mohammadrezasabramooz <34890448+mohammadrezasabramooz@users.noreply.github.com>
2020-08-19 19:38:48 +0430

    Update Address.java
    Encapsulate Address.java

--- MiniJava/src/main/java/codeGenerator/Address.java
+++ MiniJava/src/main/java/codeGenerator/Address.java
@@ -4,11 +4,35 @@ package codeGenerator;
  * Created by mohammad hosein on 6/28/2015.
  */
 public class Address {
-    public int num;
-    public TypeAddress Type;
-    public VarType varType;
+    private int num;
+    private TypeAddress Type;
+    private VarType varType;
 
-    public Address(int num,VarType varType, TypeAddress Type) {
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public TypeAddress getType() {
+        return Type;
+    }
+
+    public void setType(TypeAddress type) {
+        Type = type;
+    }
+
+    public VarType getVarType() {
+        return varType;
+    }
+
+    public void setVarType(VarType varType) {
+        this.varType = varType;
+    }
+
+    public Address(int num, VarType varType, TypeAddress Type) {
         this.num = num;
         this.Type = Type;
         this.varType = varType;
