a104aa423c68716d78b55b31dbf88437ee9964cc
mohammadrezasabramooz <34890448+mohammadrezasabramooz@users.noreply.github.com>
2020-08-19 14:36:23 +0430

    Update Parser.java

--- MiniJava/src/main/java/parser/Parser.java
+++ MiniJava/src/main/java/parser/Parser.java
@@ -7,9 +7,9 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Stack;
 
-import Log.Log;
+import Log.LogUtils;
 import codeGenerator.CodeGenerator;
-import errorHandler.ErrorHandler;
+import errorHandler.ErrorhandlerHelper;
 import scanner.lexicalAnalyzer;
 import scanner.token.Token;
 
@@ -47,10 +47,10 @@ public class Parser {
     Action currentAction;
     while (!finish) {
       try {
-        Log.print(/*"lookahead : "+*/ lookAhead.toString() + "\t" + parsStack.peek());
+        LogUtils.print(/*"lookahead : "+*/ lookAhead.toString() + "\t" + parsStack.peek());
 //                Log.print("state : "+ parsStack.peek());
         currentAction = parseTable.getActionTable(parsStack.peek(), lookAhead);
-        Log.print(currentAction.toString());
+        LogUtils.print(currentAction.toString());
         //Log.print("");
 
         switch (currentAction.action) {
@@ -65,22 +65,22 @@ public class Parser {
               parsStack.pop();
             }
 
-            Log.print(/*"state : " +*/ parsStack.peek() + "\t" + rule.LHS);
+            LogUtils.print(/*"state : " +*/ parsStack.peek() + "\t" + rule.LHS);
 //                        Log.print("LHS : "+rule.LHS);
             parsStack.push(parseTable.getGotoTable(parsStack.peek(), rule.LHS));
-            Log.print(/*"new State : " + */parsStack.peek() + "");
+            LogUtils.print(/*"new State : " + */parsStack.peek() + "");
 //                        Log.print("");
             try {
               cg.semanticFunction(rule.semanticAction, lookAhead);
             } catch (Exception e) {
-              Log.print("Code Genetator Error");
+              LogUtils.print("Code Genetator Error");
             }
             break;
           case accept:
             finish = true;
             break;
         }
-        Log.print("");
+        LogUtils.print("");
 
       } catch (Exception ignored) {
 
@@ -104,7 +104,7 @@ public class Parser {
 
 
     }
-    if (!ErrorHandler.hasError){
+    if (!ErrorhandlerHelper.hasError){
       cg.printMemory();
     }
 
@@ -113,3 +113,4 @@ public class Parser {
 
 
 }
+
