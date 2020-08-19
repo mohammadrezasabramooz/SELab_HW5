abd5bd93b39610271f0a4c6b97ef36e4187ca9a6
mohammadrezasabramooz <34890448+mohammadrezasabramooz@users.noreply.github.com>
2020-08-19 14:37:20 +0430

    Update and rename Log.java to LogUtils.java

--- MiniJava/src/main/java/Log/LogUtils.java
+++ MiniJava/src/main/java/Log/LogUtils.java
@@ -3,7 +3,7 @@ package Log;
 /**
  * Created by mohammad hosein on 6/29/2015.
  */
-public class Log {
+public class LogUtils {
     public static void print(String s)
     {
 //        System.out.println(s);
